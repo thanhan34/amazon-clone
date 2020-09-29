5358beaaaab43960eccbc5439a59d963ff27743f
An Doan <dtan42@gmail.com>
2020-09-30 01:49:56 +1000

    remove item from basket

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,14 @@
 
 export const initialState = {
-    basket: [],
+    basket: [
+        {
+            id: "123131",
+            title: "Garmin 010-01614-00 Forerunner 735XT, Black/Gray",
+            image: "https://images-fe.ssl-images-amazon.com/images/I/31IucGwsjEL._AC_AA240_FMwebp_QL65_.jpg",
+            price: 299.00,
+            rating: 5,
+        }
+    ],
 };
 export const actionTypes = {
     ADD_TO_BASKET: "ADD_TO_BASKET",
@@ -17,7 +25,15 @@ const reducer = (state, action) => {
             }
             break;
         case actionTypes.REMOVE_FROM_BASKET:
-            return { state }
+            let newBasket = [...state.basket];
+            const index = state.basket.findIndex((basketItem) => basketItem.id === action.id);
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            }
+            else {
+                console.warn(`Cant remove product(id:${action.id}) as its not exist`)
+            }
+            return { ...state, basket: newBasket, }
             break;
         default:
             return state;
